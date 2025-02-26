614abf7cd64c5d966e9ade4ab85fd6b645909bbe
jie.jiang <jiecfd@gmail.com>
2025-02-26 11:45:58 +0800

    阅读rdma-example

--- common.h
+++ common.h
@@ -8,7 +8,7 @@ inline const fuse_ino_t kRootInode = 1;
 inline const int kThreadCnt = 4;
 inline const int kMemBlockSize = 64 * 1024 * 1024;
 
-static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
+inline int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
                              off_t off, size_t maxsize) {
   if (off < bufsize)
     return fuse_reply_buf(req, buf + off, std::min(bufsize - off, maxsize));
